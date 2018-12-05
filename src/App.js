032700ace8abbdfde9886f84a366f04f5a29027a
Marshmallow <763301256@qq.com>
2018-12-05 11:28:42 +0800

    上传

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Tabs from './demo/tabs/index'
-import Upload from './demo/qiniuUpload/index'
+// import Upload from './demo/qiniuUpload/index'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Upload/>
+        <Tabs/>
       </div>
     );
   }
--- src/demo/qiniuUpload/qiniu-upload.js
+++ src/demo/qiniuUpload/qiniu-upload.js
@@ -16,94 +16,91 @@ export default class extends Component {
         let num = imgBox[0].getElementsByClassName('am-image-picker-list')[0].childNodes.length;
         if (this.state.rowNum !== num) {
             this.setState({rowNum: num});
-            // this.onLoadImg();
+            this.onLoadImg();
         }
         else if (this.state.isLastPosition || this.state.modal) {
-            // this.onLoadImg();
+            this.onLoadImg();
         }
     }
 
     /**
      * baseURL请填写自己的url
      */
-    // onLoadImg = () => {
-    //     let _this = this;
-    //     let imgBox = document.getElementsByClassName(this.props.name);
-    //     console.log(imgBox[0]);
-    //     if (imgBox[0].getElementsByTagName("input")[0] !== undefined) {
-    //         imgBox[0].getElementsByTagName("input")[0].setAttribute('id', `${_this.props.inputID}${_this.props.files.length / 4}`);
-    //         var Qiniu1 = new QiniuJsSDK();
-    //         var Qiniu2 = new QiniuJsSDK();
-    //         var commonOption = {
-    //             runtimes: 'html5,flash,html4',      // 上传模式,依次退化
-    //             uptoken_url: 'http://171.168.1.200/quantacircle/' + 'free/getQiNiuToken',    // Ajax 请求 uptoken 的 Url，**强烈建议设置**（服务端提供）
-    //             get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的 uptoken
-    //             unique_names: true,              // 默认 false，key 为文件名。若开启该选项，JS-SDK 会为每个文件自动生成key（文件名）
-    //             domain: 'image.quantahelp.com',     // bucket 域名，下载资源时用到，如：'http://xxx.bkt.clouddn.com/' **必需**
-    //             auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传,
-    //             multi_selection: false,             //禁止选择多个文件
-    //         }
-    //         var init1 = {
-    //             init: {
-    //                 'FilesAdded': function (up, files) {
-    //                     // 设置预览图地址
-    //                 },
-    //                 'FileUploaded': function (up, file, info) {
-    //                     var domain = up.getOption('domain');
-    //                     var res = JSON.parse(info);
-    //                     var sourceLink = "http://" + domain + "/" + res.key;//目标地址
-    //                     console.info(sourceLink);
-    //                     console.log(_this.props.name);
-    //                     _this.props.saveImg(_this.props.name, sourceLink);
-    //                     _this.setState({modal: false});
-    //                 },
-    //                 'Error': function (up, err, errTip) {
-    //                     //上传出错时，处理相关的事情
-    //                     console.log(err);
-    //                 },
-    //                 'Key': function (up, file) {
-    //                     var key = "";
-    //                     return key
-    //                 }
-    //             }
-    //         }
-    //         var init2 = {
-    //             init: {
-    //                 'FilesAdded': function (up, files) {
-    //                     // 设置预览图地址
-    //                 },
-    //                 'FileUploaded': function (up, file, info) {
-    //                     var domain = up.getOption('domain');
-    //                     var res = JSON.parse(info);
-    //                     var sourceLink = "http://" + domain + "/" + res.key;//目标地址
-    //                     console.info(sourceLink);
-    //                     console.log(_this.props.name);
-    //                     _this.props.saveImg(_this.props.name, sourceLink);
-    //                     _this.setState({modal: false});
-    //                 },
-    //                 'Error': function (up, err, errTip) {
-    //                     //上传出错时，处理相关的事情
-    //                     console.log(err);
-    //                 },
-    //                 'Key': function (up, file) {
-    //                     var key = "";
-    //                     return key
-    //                 }
-    //             }
-    //         }
-    //         // var aimId2={browse_button: `${_this.props.inputID}${_this.props.files.length/4}`};
-    //         // var option2 = Object.assign(commonOption, init2,aimId2);
-    //         // var uploader1 = Qiniu2.uploader(option2);
-    //         var aimId1 = {browse_button: `${this.props.inputID}134`};
-    //         var option1 = Object.assign(commonOption, init1, aimId1);
-    //         var uploader1 = Qiniu1.uploader(option1);
-    //
-    //         var aimId2 = {browse_button: `${this.props.inputID}135`};
-    //         var option2 = Object.assign(commonOption, init2, aimId2);
-    //         var uploader = Qiniu2.uploader(option2);
-    //         setTimeout(() => this.setInput(1), 500)
-    //     }
-    // };
+    onLoadImg = () => {
+        let _this = this;
+        let imgBox = document.getElementsByClassName(this.props.name);
+        console.log(imgBox[0]);
+        if (imgBox[0].getElementsByTagName("input")[0] !== undefined) {
+            imgBox[0].getElementsByTagName("input")[0].setAttribute('id', `${_this.props.inputID}${_this.props.files.length / 4}`);
+            var Qiniu1 = new QiniuJsSDK();
+            var Qiniu2 = new QiniuJsSDK();
+            var commonOption = {
+                runtimes: 'html5,flash,html4',      // 上传模式,依次退化
+                uptoken_url: baseURL + 'free/getQiNiuToken',    // Ajax 请求 uptoken 的 Url，**强烈建议设置**（服务端提供）
+                get_new_uptoken: false,             // 设置上传文件的时候是否每次都重新获取新的 uptoken
+                unique_names: true,              // 默认 false，key 为文件名。若开启该选项，JS-SDK 会为每个文件自动生成key（文件名）
+                domain: 'image.quantahelp.com',     // bucket 域名，下载资源时用到，如：'http://xxx.bkt.clouddn.com/' **必需**
+                auto_start: true,                   // 选择文件后自动上传，若关闭需要自己绑定事件触发上传,
+                multi_selection: false,             //禁止选择多个文件
+            }
+            var init1 = {
+                init: {
+                    'FilesAdded': function (up, files) {
+                        // 设置预览图地址
+                    },
+                    'FileUploaded': function (up, file, info) {
+                        var domain = up.getOption('domain');
+                        var res = JSON.parse(info);
+                        var sourceLink = "http://" + domain + "/" + res.key;//目标地址
+                        console.info(sourceLink);
+                        console.log(_this.props.name);
+                        _this.props.saveImg(_this.props.name, sourceLink);
+                        _this.setState({modal: false});
+                    },
+                    'Error': function (up, err, errTip) {
+                        //上传出错时，处理相关的事情
+                        console.log(err);
+                    },
+                    'Key': function (up, file) {
+                        var key = "";
+                        return key
+                    }
+                }
+            }
+            var init2 = {
+                init: {
+                    'FilesAdded': function (up, files) {
+                        // 设置预览图地址
+                    },
+                    'FileUploaded': function (up, file, info) {
+                        var domain = up.getOption('domain');
+                        var res = JSON.parse(info);
+                        var sourceLink = "http://" + domain + "/" + res.key;//目标地址
+                        console.info(sourceLink);
+                        console.log(_this.props.name);
+                        _this.props.saveImg(_this.props.name, sourceLink);
+                        _this.setState({modal: false});
+                    },
+                    'Error': function (up, err, errTip) {
+                        //上传出错时，处理相关的事情
+                        console.log(err);
+                    },
+                    'Key': function (up, file) {
+                        var key = "";
+                        return key
+                    }
+                }
+            }
+            var aimId1 = {browse_button: `${this.props.inputID}134`};
+            var option1 = Object.assign(commonOption, init1, aimId1);
+            var uploader1 = Qiniu1.uploader(option1);
+
+            var aimId2 = {browse_button: `${this.props.inputID}135`};
+            var option2 = Object.assign(commonOption, init2, aimId2);
+            var uploader = Qiniu2.uploader(option2);
+            setTimeout(() => this.setInput(1), 500)
+        }
+    };
 
     setInput = (n) => {
         let number = n + 1;
